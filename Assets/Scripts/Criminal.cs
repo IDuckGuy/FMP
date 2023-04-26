e2d814a3c37d505439a00ef1a6b0f5fd2dca551f
ARM20005877 - Oliver Armstrong <ARM20005877@wakecoll.net>
2023-04-26 14:39:43 +0100

    Auto commit

---
+++ Assets/Scripts/Criminal.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Criminal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public Transform Obj;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Timer());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(3);
+        Instantiate(Obj, transform.position, transform.rotation);
+        StartCoroutine(Timer());
     }
 }
